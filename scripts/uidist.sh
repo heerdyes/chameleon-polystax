fba009ea765a852fd8d3f43622085cebbf051f73
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2023-08-22 21:19:02 +0530

    scripts for deploying frontends into backends

--- scripts/uidist.sh
+++ scripts/uidist.sh
@@ -1,6 +1,6 @@
 # copy react dist files into spring backend static dir
-REACTUI="frontends/react/zeroth"
-SPRINGBE="backends/spring/zeroth"
+REACTUI="../frontends/react/zeroth"
+SPRINGBE="../backends/spring/zeroth"
 BESTATIC=$SPRINGBE/src/main/resources/static
 UIDIST=$REACTUI/dist
 
